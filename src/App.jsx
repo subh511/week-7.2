3dd042d75ce70a3c033765165d5fb260e8f463da
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-15 19:44:56 +0530

    started navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
-import './App.css'
-
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div>
+    <button>Home </button>
+    <button>My Network ()</button>
+    <button>Jobs ()</button>
+    <button>Messaging ()</button>
+    <button>Notification ()</button>
+    <button>Me ()</button>
     </div>
   )
 }
